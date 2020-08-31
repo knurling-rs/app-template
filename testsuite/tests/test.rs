f13fab5841cd4456ac1dceef3eb8a9c43e050dbb
Jonas Schievink <jonas.schievink@ferrous-systems.com>
2020-08-31 19:16:37 +0200

    project -> crate name

--- testsuite/tests/test.rs
+++ testsuite/tests/test.rs
@@ -2,11 +2,11 @@
 #![no_main]
 
 use cortex_m_rt::entry;
-use {{project-name}} as _; // memory layout + panic handler
+use {{crate_name}} as _; // memory layout + panic handler
 
 #[entry]
 fn main() -> ! {
     assert!(false, "TODO: Write actual tests");
 
-    {{project-name}}::exit();
+    {{crate_name}}::exit();
 }
