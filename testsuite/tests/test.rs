fbd9d9313851dc05523d429169fb35ad7233a80b
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2020-11-13 15:45:03 +0100

    use defmt-test in the test suite

--- testsuite/tests/test.rs
+++ testsuite/tests/test.rs
@@ -4,7 +4,8 @@
 use cortex_m_rt::entry;
 use {{crate_name}} as _; // memory layout + panic handler
 
-// See https://crates.io/crates/defmt-test/0.1.0 for more documentation
+// See https://crates.io/crates/defmt-test/0.1.0 for more documentation (e.g. about the 'state'
+// feature)
 #[defmt_test::tests]
 mod tests {
     #[test]
