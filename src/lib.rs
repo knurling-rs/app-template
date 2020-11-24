ae0df60ec2624564311cb4ac4732d7f4c1204d50
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2020-11-24 17:21:15 +0100

    use defmt::assert; add defmt::panic_handler

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,17 @@
 use core::sync::atomic::{AtomicUsize, Ordering};
 
 use defmt_rtt as _; // global logger
-use panic_probe as _;
 // TODO(5) adjust HAL import
 // use some_hal as _; // memory layout
+
+use panic_probe as _;
+
+// same panicking *behavior* as `panic-probe` but doesn't print a panic message
+// this prevents the panic message being printed *twice* when `defmt::panic` is invoked
+#[defmt::panic_handler]
+fn panic() -> ! {
+    cortex_m::asm::udf()
+}
 
 #[defmt::timestamp]
 fn timestamp() -> u64 {
