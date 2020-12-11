2a34a5279da594092e6069fbb604c3040be0a5b6
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2020-12-11 12:29:03 +0100

    testsuite: import defmt assert macros
    these imports will override the ones from the core prelude

--- testsuite/tests/test.rs
+++ testsuite/tests/test.rs
@@ -7,13 +7,15 @@ use {{crate_name}} as _; // memory layout + panic handler
 // feature)
 #[defmt_test::tests]
 mod tests {
+    use defmt::{assert, assert_eq};
+
     #[test]
     fn assert_true() {
-        defmt::assert!(true)
+        assert!(true)
     }
 
     #[test]
     fn assert_eq() {
-        defmt::assert_eq!(24, 42, "TODO: write actual tests")
+        assert_eq!(24, 42, "TODO: write actual tests")
     }
 }
