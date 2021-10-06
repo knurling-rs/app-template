8fe11fcd9a447bbb180765833250446a8cbe0e3b
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2021-10-06 11:56:27 +0200

    remove `timestamp!`
    it's no longer mandatory and a monotonic counter does not add much value given that `defmt-rtt`
    works by disabling interrupts so one won't see logs out of order anyways (which would the case for a
    lock-free multi-channel `Logger`)

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 #![no_std]
 
-use core::sync::atomic::{AtomicUsize, Ordering};
-
 use defmt_rtt as _; // global logger
+
 // TODO(5) adjust HAL import
 // use some_hal as _; // memory layout
 
@@ -14,14 +13,6 @@ use panic_probe as _;
 fn panic() -> ! {
     cortex_m::asm::udf()
 }
-
-static COUNT: AtomicUsize = AtomicUsize::new(0);
-defmt::timestamp!("{=usize}", {
-    // NOTE(no-CAS) `timestamps` runs with interrupts disabled
-    let n = COUNT.load(Ordering::Relaxed);
-    COUNT.store(n + 1, Ordering::Relaxed);
-    n
-});
 
 /// Terminates the application and makes `probe-run` exit with exit-code = 0
 pub fn exit() -> ! {
